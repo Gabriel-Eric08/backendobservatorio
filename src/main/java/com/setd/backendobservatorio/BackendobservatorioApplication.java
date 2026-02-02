992d88f2713099360310744dc29f6d6d27f30ab1
Gabriel Eric <gabriel.eric@ufrpe.br>
2026-02-01 21:21:19 -0300

    :tada: start commit

---
+++ src/main/java/com/setd/backendobservatorio/BackendobservatorioApplication.java
@@ -0,0 +1,13 @@
+package com.setd.backendobservatorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendobservatorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendobservatorioApplication.class, args);
+	}
+
+}
